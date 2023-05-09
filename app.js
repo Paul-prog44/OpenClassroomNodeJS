e49ad36052c67deb1db6ad9ead65a5a365d33ef5
Paul-prog44 <saintouenfr@hotmail.com>
2023-05-09 16:59:36 +0200

    Add sign in function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 
 const stuffRoutes = require('./routes/stuff')
+const userRoutes = require('./routes.user')
 
 const app = express()
 mongoose.connect('mongodb+srv://user:user@cluster0.lpspbwo.mongodb.net/?retryWrites=true&w=majority',
@@ -24,5 +25,6 @@ app.use((req, res, next) => {
  app.use(bodyParser.json())
 
  app.use('/api/stuff', stuffRoutes)
+ app.use('/api/auth', userRoutes)
 
 module.exports = app
